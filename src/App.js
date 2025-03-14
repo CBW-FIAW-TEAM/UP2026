c1faab66e64e4c1952dd3f45ac2929cceaa1d7ae
Artur Sabanadze <diakosmisi26@hotmail.com>
2025-03-14 17:49:21 +0100

    www

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/footer";
+ 
+function App() {
+  return (
+<div>
+<h1>Mein Umfrage-Tool</h1>
+<Main />
+<Footer />
+</div>
+  );
+}
+ 
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+function Main() {
+  return (
+<div>
+<h2>Willkommen zur Online-Umfrage!</h2>
+<p>Bitte beantworte die folgenden Fragen.</p>
+</div>
+  );
+}
+ 
+export default Main;
